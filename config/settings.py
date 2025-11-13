43665458db6970612583e8f29d63d494c577a595
VenEl101 <newsacc31@gmail.com>
2025-11-13 16:09:40 +0500

    test14

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': "deployment_app",
         'USER': "postgres",
         'PASSWORD': "newpassword",
-        'HOST': "localhost",
+        "HOST": "127.0.0.1",
         'PORT': 5432,
     }
 }
